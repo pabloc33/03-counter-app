a7097b3a4a16d0efd1bb14522df33e0e21a73cb4
pabloc33 <68171460+pabloc33@users.noreply.github.com>
2020-08-20 22:23:03 -0300

    cambio1

---
+++ src/PrimeraApp.js
@@ -0,0 +1,27 @@
+//import React, { Fragment } from "react";
+import React from "react";
+import PropTypes from "prop-types";
+
+//FC
+
+const PrimeraApp = ({ saludo, subtitulo }) => {
+  //const saludo = "Hola Mundo";
+
+  return (
+    <>
+      <h1>{saludo}</h1>
+      {/*<h1>{JSON.stringify(saludo, null, 3)}</h1>*/}
+      <p>{subtitulo}</p>
+    </>
+  );
+};
+
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string.isRequired,
+};
+
+PrimeraApp.defaultProps = {
+  subtitulo: "Soy un subtitulo",
+};
+
+export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+//import PrimeraApp from "./PrimeraApp";
+import CounterApp from "./CounterApp";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const divRoot = document.querySelector("#root");
+
+ReactDOM.render(<CounterApp value={123} />, divRoot);
