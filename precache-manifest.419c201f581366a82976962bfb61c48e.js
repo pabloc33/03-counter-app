7f25e161908d01a094d49ba14e4b3bb2283099e8
pabloc33 <68171460+pabloc33@users.noreply.github.com>
2020-08-22 15:25:17 -0300

    Updates

---
+++ precache-manifest.419c201f581366a82976962bfb61c48e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6773e03cae1c2b37f0fdfc290178347",
+    "url": "/03-counter-app/index.html"
+  },
+  {
+    "revision": "95aca7baf9f648173fc9",
+    "url": "/03-counter-app/static/css/main.71cd56a4.chunk.css"
+  },
+  {
+    "revision": "9394425a6b8574bfd50e",
+    "url": "/03-counter-app/static/js/2.87faeb51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/03-counter-app/static/js/2.87faeb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95aca7baf9f648173fc9",
+    "url": "/03-counter-app/static/js/main.fc440db8.chunk.js"
+  },
+  {
+    "revision": "2e9c14ac395a8ba28604",
+    "url": "/03-counter-app/static/js/runtime-main.c2b42fda.js"
+  }
+]);
