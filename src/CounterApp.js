8f64c243ae92b908f3b5f67f9677e9801e1745e2
pabloc33 <68171460+pabloc33@users.noreply.github.com>
2020-08-22 12:39:47 -0300

    Cambio 2

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -22,7 +22,7 @@ const CounterApp = ({ value = 10 }) => {
     <>
       <h1>CounterApp</h1>
       <h2> {counter} </h2>
-      {/* cAMBIO 2 */}
+      {/* Cambio 2 */}
       <button onClick={handleAdd}>+1</button>
       <button onClick={handleReset}>Reset</button>
       <button onClick={handleSubtract}>-1</button>
